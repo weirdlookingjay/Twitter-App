032e770fb41d95765eeb5eca4a84873d1f3887c2
Joseph Clark <jclark@awinyc.americaworks.com>
2022-04-11 10:36:52 -0400

    Created User, Tweet and Follow classes

---
+++ core/classes/User.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class User
+{
+	protected $pdo;
+
+	function __construct($pdo) {
+		$this->pdo = $pdo;
+	}
+}
--- core/database/connection.php
+++ core/database/connection.php
@@ -1,2 +1,11 @@
 <?php
 
+$dsn = 'mysql:host=localhost; dbname=twitter';
+$user = 'root';
+$pass = 'r00tadmin';
+
+try {
+	$pdo = PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+	echo 'Connection error! ' . $e->getMessage();
+}
